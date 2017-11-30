d728c310e660d4a64b76caf9f2403e332e4c0116
RainingFlamebows <sshih16@berkeley.edu>
2017-11-30 01:23:24 -0800

    Was able to solve almost all 20,35,50 input files through random black magic fuckery

--- swap.py
+++ swap.py
@@ -1,5 +1,9 @@
+import math 
 import random
+import time
+import pdb
 
+all_constraints = []
 def solve(num_wizards, num_constraints, wizards, constraints):
     """
     Write your algorithm here.
@@ -13,41 +17,84 @@ def solve(num_wizards, num_constraints, wizards, constraints):
     Output:
         An array of wizard names in the ordering your algorithm returns
     """ 
+    global all_constraints
+    all_constraints = constraints
 
     # number of unsatisfied constraints we're okay with
-    max_unsatisfied = 10
     ordering = genSeedOrder(wizards, constraints) 
+    best_ordering = ordering
     num_sat = 0
-    while num_sat != num_constraints: 
-        # swap
-        num_sat = 0
-        for const in constraints : 
-            if isOrderingValid(ordering, const) : 
-                num_sat += 1
+    best_sat = 0
+    unsat = findUnsatisfied(ordering, constraints)
+    best_unsat = len(unsat)
+
+    temp = 1
+    alpha = 0.99
+    while num_sat < num_constraints: 
+        for i in range(500) : 
+            ordering = best_ordering
+            unsat = findUnsatisfied(ordering, constraints)
+            if (len(unsat) == 0) : 
+                return ordering
+            const = random.choice(unsat)
+
+            a, b, c = const
+            order1 = ordering[:]
+            order1[ordering.index(c)] = a
+            order1[ordering.index(a)] = c
+            unsat1 = findUnsatisfied(order1, constraints)
+
+            order2 = ordering[:]
+            order2[ordering.index(c)] = b
+            order2[ordering.index(b)] = c
+            unsat2 = findUnsatisfied(order2, constraints)
+            if (len(unsat1) < len(unsat2)) : 
+                ordering = order1
+                unsat = unsat1
             else : 
-                # a, b, c = unsatisfied[random.randint(0, len(unsatisfied) - 1)]
-                a, b, c = const
-                order1 = ordering[:]
-                order1[order1.index(c)] = a
-                order1[order1.index(a)] = c
-                # order1_unsat = len(findUnsatisfied(order1, constraints))
-                order2 = ordering[:]
-                order2[order2.index(c)] = b
-                order2[order2.index(b)] = c
-                # order2_unsat = len(findUnsatisfied(order2, constraints))
+                ordering = order2
+                unsat = unsat2
 
+            num_unsat = len(unsat)
+            num_sat = num_constraints - len(unsat)
+            # print("ordering", ordering, "satisfies", num_sat)
 
-                num = random.randint(0, 1)
-                if num == 0 : 
-                    ordering = order1
-                else : 
-                    ordering = order2
-        print(num_sat)
-    return ordering
+            if (num_unsat < best_unsat) : 
+                print("best_unsat - num_unsat", best_unsat - num_unsat)
+                
+            accept_prob = math.exp((best_unsat - num_unsat)/math.sqrt(temp))
+            if accept_prob > random.random() : 
+                best_ordering = ordering[:]
+                best_sat = num_sat
+                best_unsat = len(unsat)
+                print("best ordering", best_ordering, "satisfies", best_sat)
+        temp += 1
+    return best_ordering
         
+def swap(ordering, const) : 
+    a, b, c = const
+    ordering.remove(c)
+    aIndex = ordering.index(a)
+    bIndex = ordering.index(b)
+    # allNewCIndexes = [aIndex, bIndex]
+    ordering.insert(aIndex, c)
+    unsat1 = findUnsatisfied(ordering, all_constraints)
+    ordering.remove(c)
+
+    ordering.insert(bIndex, c)
+    unsat2 = findUnsatisfied(ordering, all_constraints)
+    ordering.remove(c)
+
+    if (len(unsat1) < len(unsat2)) : 
+        ordering.insert(aIndex, c)
+        return unsat1
+    else : 
+        ordering.insert(bIndex, c)
+        return unsat2
+
 
 def genSeedOrder(wizards, constraints) : 
-    random.shuffle(wizards)
+    # random.shuffle(wizards)
     return wizards
 
 def findUnsatisfied(ordering, constraints) : 
