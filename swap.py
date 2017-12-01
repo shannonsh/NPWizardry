0efbf33f3ec48ce3688e9627c092eac0beca4570
RainingFlamebows <sshih16@berkeley.edu>
2017-11-30 17:47:33 -0800

    Questionable improvement: decrease likelihood of switching to a seen ordering if seen many times

--- swap.py
+++ swap.py
@@ -1,7 +1,6 @@
 import math 
+import numpy
 import random
-import time
-import pdb
 
 all_constraints = []
 def solve(num_wizards, num_constraints, wizards, constraints):
@@ -20,22 +19,22 @@ def solve(num_wizards, num_constraints, wizards, constraints):
     global all_constraints
     all_constraints = constraints
 
-    seen = set([])
+    seen = {}
 
     # number of unsatisfied constraints we're okay with
     ordering = genSeedOrder(wizards, constraints) 
     best_ordering = ordering
-    seen.add(tuple(best_ordering))
+    addToDict(seen, tuple(best_ordering))
     num_sat = 0
     best_sat = 0
     unsat = findUnsatisfied(ordering, constraints)
     best_unsat = len(unsat)
 
     temp = 1
-    # rate of decrease in randomness
-    alpha = 0.999
     accept_prob = 1 
     while num_sat < num_constraints: 
+        randList = numpy.random.uniform(size=500)
+        randList2 = numpy.random.uniform(size=500)
         for i in range(500) : 
             ordering = best_ordering
             unsat = findUnsatisfied(ordering, constraints)
@@ -59,26 +58,16 @@ def solve(num_wizards, num_constraints, wizards, constraints):
                 if (len(best_swap_unsat) > len(unsat1)) : 
                     # if ordering seen before, probability of accepting it decreases over time
                     # reduces probability of getting stuck in a local minima
-                    if (tuple(order1) not in seen or accept_prob > random.random()) : 
+                    if tuple(order1) in seen : 
+                        accept_prob = math.exp((best_unsat - num_unsat)/math.sqrt(seen[tuple(order1)]))
+                    if (tuple(order1) not in seen or accept_prob > randList2[i]) : 
                         best_swap = order1 
                         best_swap_unsat = unsat1
             ordering = best_swap
             unsat = best_swap_unsat
 
-#             order2 = ordering[:]
-#             order2[ordering.index(c)] = b
-#             order2[ordering.index(b)] = c
-#             unsat2 = findUnsatisfied(order2, constraints)
-#             if (len(unsat1) < len(unsat2)) : 
-#                 ordering = order1
-#                 unsat = unsat1
-#             else : 
-#                 ordering = order2
-#                 unsat = unsat2
-
             num_unsat = len(unsat)
             num_sat = num_constraints - len(unsat)
-            # print("ordering", ordering, "satisfies", num_sat)
 
             if (num_unsat < best_unsat) : 
                 print("best_unsat - num_unsat", best_unsat - num_unsat)
@@ -87,9 +76,9 @@ def solve(num_wizards, num_constraints, wizards, constraints):
             accept_prob = math.exp((best_unsat - num_unsat)/math.sqrt(temp))
             # if new solution better than best solution then definitely update
             # best solution
-            if accept_prob > random.random() : 
+            if accept_prob > randList[i] : 
                 best_ordering = ordering[:]
-                seen.add(tuple(best_ordering))
+                addToDict(seen, tuple(best_ordering))
                 best_sat = num_sat
                 best_unsat = len(unsat)
                 print("best ordering", best_ordering, "satisfies", best_sat)
@@ -119,3 +108,8 @@ def isOrderingValid(ordering, constraint) :
         return False
     return True
 
+def addToDict(seen, ordering) : 
+	if (ordering in seen) : 
+		seen[ordering] += 1
+	else : 
+		seen[ordering] = 1
